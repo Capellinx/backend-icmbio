08a32b43f28e3cc86bea7e027ccf5d7ba5392257
Capellinx <capellaaa7@gmail.com>
2024-08-02 01:35:03 -0300

    feat: create new control error (zod)

--- src/middlewares/handle-error.middleware.ts
+++ src/middlewares/handle-error.middleware.ts
@@ -2,6 +2,7 @@ import "express-async-errors"
 import { NextFunction, Request, Response } from "express";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { ApiError } from "../config/error/api.error";
+import { ZodError } from "zod";
 
 export class handleErrors {
    static execute(error: Error, req: Request, res: Response, next: NextFunction) {
@@ -11,6 +12,10 @@ export class handleErrors {
       }
 
       if (error instanceof PrismaClientKnownRequestError) {
+         return res.status(500).json({ error: 'Internal Server Error.' });
+      }
+
+      if(error instanceof ZodError){
          return res.status(500).json({ error: 'Internal Server Error.' });
       }
 
