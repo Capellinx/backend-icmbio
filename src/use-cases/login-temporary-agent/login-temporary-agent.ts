dd7a279b65d634a6d23c832577d54e076e5bca3d
Capellinx <capellaaa7@gmail.com>
2024-08-02 00:59:33 -0300

    feat: create use case login

---
+++ src/use-cases/login-temporary-agent/login-temporary-agent.ts
@@ -0,0 +1,34 @@
+import { PasswordAtuhGuard } from "../../auth/password.auth";
+import { TokenAuthGuard } from "../../auth/token.auth";
+import { ApiError } from "../../config/error/api.error";
+import { ILoginTemporaryAgentRequestDTO } from "../../domain/DTOs/temporaryAgent.DTO";
+import { ITemporaryAgentRepository } from "../../repositories/temporaryAgent.repository";
+
+export class LoginTemporaryAgentUseCase {
+   constructor(
+      private temporaryAgentRepository: ITemporaryAgentRepository
+   ) { }
+
+   async execute({ email, password }: ILoginTemporaryAgentRequestDTO): Promise<any> {
+      const temporaryAgentExists = await this.temporaryAgentRepository.findByEmail(email)
+
+      if(!temporaryAgentExists?.email){
+         return new ApiError("Temporary agent not found", 404)
+      }
+
+      const passwordMatch = await PasswordAtuhGuard.comparePassword(password, temporaryAgentExists.password)
+
+      if(!passwordMatch){
+         return new ApiError("Incorrect password", 401)
+      }
+      
+      const token = await TokenAuthGuard.generateToken(temporaryAgentExists.id)
+
+      return {
+         accessToken: token,
+         user: {
+            ...temporaryAgentExists
+         }
+      }
+   }
+}
