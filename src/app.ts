b6b742d98824c2cbed823ed95c207137c12224bb
Capellinx <capellaaa7@gmail.com>
2024-08-02 00:42:37 -0300

    feat: instanced handle errors in global app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express, { json }  from "express";
 import { temporaryAgentRouter } from "./routes/temporary-agent.routes";
+import { handleErrors } from "./middlewares/handle-error.middleware";
+import "express-async-errors"
 
 export const app = express()
 
 app.use(json())
+app.use(express.json())
+
 app.use("/users", temporaryAgentRouter)
+
+app.use(handleErrors.execute)
