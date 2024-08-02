29ff0fed243a16281c5678ad861a4be4f8488725
Capellinx <capellaaa7@gmail.com>
2024-08-02 00:42:02 -0300

    feat: create handle error global for application

---
+++ src/middlewares/handle-error.middleware.ts
@@ -0,0 +1,19 @@
+import "express-async-errors"
+import { NextFunction, Request, Response } from "express";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { ApiError } from "../config/error/api.error";
+
+export class handleErrors {
+   static execute(error: Error, req: Request, res: Response, next: NextFunction) {
+      if (error instanceof ApiError) {
+         console.log(error.message)
+         return res.status(error.statusCode).json({ message: error.message });
+      }
+
+      if (error instanceof PrismaClientKnownRequestError) {
+         return res.status(500).json({ error: 'Internal Server Error.' });
+      }
+
+      return res.status(500).json({ message: "Internal Server Error." })
+   }
+}
