5fd681a5aa6058aacdba299ec3fb54ca5112588d
Capellinx <capellaaa7@gmail.com>
2024-08-02 00:58:23 -0300

    feat: create new contract of login

--- src/repositories/temporaryAgent.repository.ts
+++ src/repositories/temporaryAgent.repository.ts
@@ -3,4 +3,5 @@ import { TemporaryAgent } from "../domain/entities/temporaryAgent"
 export interface ITemporaryAgentRepository {
    save: (temporaryAgent: TemporaryAgent) => Promise<void>
    findByEmail: (email: string) => Promise<TemporaryAgent | null>
+   login: (email: string, password: string) => Promise<TemporaryAgent | null>
 }
