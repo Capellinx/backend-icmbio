74388efc6a5876ace68bbf662ca29dcb91c14369
Capellinx <capellaaa7@gmail.com>
2024-08-02 00:58:13 -0300

    feat: create new route to login

--- src/routes/temporary-agent.routes.ts
+++ src/routes/temporary-agent.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { createTemporaryAgentController } from "../use-cases/create-temporary-agent";
-
+import { loginTemporaryAgentController } from "../use-cases/login-temporary-agent";
 
 export const temporaryAgentRouter = Router()
 
-temporaryAgentRouter.post("/", async (request, response) => {
+temporaryAgentRouter.post("/register", async (request, response) => {
    return createTemporaryAgentController.handle(request, response)
+})
+
+temporaryAgentRouter.post("/login", async (request, response) => {
+   return loginTemporaryAgentController.handle(request, response)
 })
