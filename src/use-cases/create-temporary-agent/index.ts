374a286609e77354456ae258c18bb33b6d99481f
Capellinx <capellaaa7@gmail.com>
2024-08-02 21:37:15 -0300

    feat: injection a new dependencie

--- src/use-cases/create-temporary-agent/index.ts
+++ src/use-cases/create-temporary-agent/index.ts
@@ -1,10 +1,13 @@
+import { BcryptTemporaryAgent } from "../../interfaces/implementations/bcrypt-temporary-agent";
 import { PostgresTemporaryAgent } from "../../repositories/implementations/postgres-temporary-agent";
 import { CreateTemporaryAgent } from "./create-temporary-agent";
 import { TemporaryAgentController } from "./create-temporary-agent.controller";
 
 const postgresTemporaryAgent = new PostgresTemporaryAgent()
 
-const createTemporaryAgent = new CreateTemporaryAgent(postgresTemporaryAgent)
+const bcryptTemporaryAgent = new BcryptTemporaryAgent()
+
+const createTemporaryAgent = new CreateTemporaryAgent(postgresTemporaryAgent, bcryptTemporaryAgent)
 
 const createTemporaryAgentController = new TemporaryAgentController(createTemporaryAgent)
 
