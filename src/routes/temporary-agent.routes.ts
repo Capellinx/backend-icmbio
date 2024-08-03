c2967699fe6cd39614ee01faf4adeadca5df8000
Capellinx <capellaaa7@gmail.com>
2024-08-02 23:59:40 -0300

    feat: create new route to send email

--- src/routes/temporary-agent.routes.ts
+++ src/routes/temporary-agent.routes.ts
@@ -1,21 +1,32 @@
 import { Router } from "express";
 import { createTemporaryAgentController } from "../use-cases/create-temporary-agent";
 import { loginTemporaryAgentController } from "../use-cases/login-temporary-agent";
-import { loginTemporaryAgentSchema, registerTemporaryAgentSchema } from "../schemas/temporary-agent.schema";
 import { ValidateBody } from "../middlewares/validate-body.middleware";
+import { sendMessageToResetPasswordController } from "../use-cases/send-message-to-reset-password";
+import {
+   loginTemporaryAgentSchema,
+   registerTemporaryAgentSchema,
+   sendMessageToResetPasswordSchema
+} from "../schemas/temporary-agent.schema";
 
 export const temporaryAgentRouter = Router()
 
 temporaryAgentRouter.post("/register",
-   ValidateBody.execute({body: registerTemporaryAgentSchema}),
+   ValidateBody.execute({ body: registerTemporaryAgentSchema }),
    async (request, response) => {
       return createTemporaryAgentController.handle(request, response)
    }
 )
 
 temporaryAgentRouter.post("/login",
-   ValidateBody.execute({body: loginTemporaryAgentSchema}),
+   ValidateBody.execute({ body: loginTemporaryAgentSchema }),
    async (request, response) => {
       return loginTemporaryAgentController.handle(request, response)
+   }
+)
+
+temporaryAgentRouter.post("/send-email",
+   ValidateBody.execute({ body: sendMessageToResetPasswordSchema }), async (request, response) => {
+      return sendMessageToResetPasswordController.handle(request, response)
    }
 )
