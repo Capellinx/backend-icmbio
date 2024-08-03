a22a054869ba5f133973bfe91d51b497162699d9
Capellinx <capellaaa7@gmail.com>
2024-08-03 01:09:36 -0300

    feat: create two new repository

--- src/repositories/temporaryAgent.repository.ts
+++ src/repositories/temporaryAgent.repository.ts
@@ -4,4 +4,6 @@ export interface ITemporaryAgentRepository {
    save: (temporaryAgent: TemporaryAgent) => Promise<void>
    findByEmail: (email: string) => Promise<TemporaryAgent | null>
    login: (email: string, password: string) => Promise<TemporaryAgent | null>
+   resetPassword: (id: string, password: string) => Promise<void>
+   findById: (id: string) => Promise<TemporaryAgent | null>
 }
