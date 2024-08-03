05723b2cd3c3ea91ac56a2474c378e075fc6cf3f
Capellinx <capellaaa7@gmail.com>
2024-08-02 23:08:10 -0300

    feat: create new dependencie (email)

--- src/use-cases/create-temporary-agent/create-temporary-agent.ts
+++ src/use-cases/create-temporary-agent/create-temporary-agent.ts
@@ -1,26 +1,43 @@
-import { PasswordAtuhGuard } from "../../auth/password.auth";
+import { ApiError } from "../../config/error/api.error";
 import { ICreateTemporaryAgentRequestDTO } from "../../domain/DTOs/temporaryAgent.DTO";
 import { TemporaryAgent } from "../../domain/entities/temporaryAgent";
+import { IMailProvider } from "../../interfaces/email.interface";
 import { IPasswordHash } from "../../interfaces/password.interface";
 import { ITemporaryAgentRepository } from "../../repositories/temporaryAgent.repository";
 
 export class CreateTemporaryAgent {
    constructor(
       private temporaryAgentRepository: ITemporaryAgentRepository,
-      private passwordHash: IPasswordHash
+      private passwordHash: IPasswordHash,
+      private emailProvider: IMailProvider
    ) { }
 
    async execute({ name, email, password, thematic_area, role }: ICreateTemporaryAgentRequestDTO) {
       const temporaryAgentExists = await this.temporaryAgentRepository.findByEmail(email)
 
       if (temporaryAgentExists) {
-         throw new Error("Temporary agent already exists")
+         throw new ApiError("Temporary agent already exists", 400)
       }
       
       const hashedPassword = await this.passwordHash.hashPassword(password)
 
       const newTemporaryAgent = new TemporaryAgent(name, email, hashedPassword, thematic_area, role)
 
-      await this.temporaryAgentRepository.save(newTemporaryAgent)
+      await Promise.all([
+         this.emailProvider.sendMail({
+            to: {
+               name: newTemporaryAgent.name,
+               email: newTemporaryAgent.email
+            },
+            from: {
+               name: "ICMBio",
+               email: "alcatrazes@icmbio.gov.com"
+            },
+            subject: "ICMBio - Código de acesso",
+            body: "ICMBio"
+         }),
+   
+         this.temporaryAgentRepository.save(newTemporaryAgent)
+      ])
    }
 }
