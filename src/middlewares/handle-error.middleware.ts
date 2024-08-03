d1c8c25bbab5474eb31b7a3462949bb2b906b709
Capellinx <capellaaa7@gmail.com>
2024-08-02 21:16:17 -0300

    feat: create a new controller error

--- src/middlewares/handle-error.middleware.ts
+++ src/middlewares/handle-error.middleware.ts
@@ -1,24 +1,22 @@
-import "express-async-errors"
 import { NextFunction, Request, Response } from "express";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { ApiError } from "../config/error/api.error";
 import { ZodError } from "zod";
 
 export class handleErrors {
-   static execute(error: Error, req: Request, res: Response, next: NextFunction) {
+   static execute(error: ApiError | Error, req: Request, res: Response, next: NextFunction) {
       if (error instanceof ApiError) {
-         console.log(error.message)
-         return res.status(error.statusCode).json({ message: error.message });
+         return res.status(error.statusCode).json({ error: error.message });
       }
 
       if (error instanceof PrismaClientKnownRequestError) {
          return res.status(500).json({ error: 'Internal Server Error.' });
       }
 
-      if(error instanceof ZodError){
-         return res.status(500).json({ error: 'Internal Server Error.' });
+      if (error instanceof ZodError) {
+         return res.status(500).json({ error: error.errors });
       }
-
+      
       return res.status(500).json({ message: "Internal Server Error." })
    }
 }
