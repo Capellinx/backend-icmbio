bceae3159c77e249e0dab3dba527c14c2a2b09d9
Capellinx <capellaaa7@gmail.com>
2024-08-02 23:09:13 -0300

    feat: inject new dependecie

--- src/use-cases/create-temporary-agent/index.ts
+++ src/use-cases/create-temporary-agent/index.ts
@@ -1,4 +1,5 @@
 import { BcryptTemporaryAgent } from "../../interfaces/implementations/bcrypt-temporary-agent";
+import { EmailMailTrap } from "../../interfaces/implementations/email-mail-trap";
 import { PostgresTemporaryAgent } from "../../repositories/implementations/postgres-temporary-agent";
 import { CreateTemporaryAgent } from "./create-temporary-agent";
 import { TemporaryAgentController } from "./create-temporary-agent.controller";
@@ -7,7 +8,13 @@ const postgresTemporaryAgent = new PostgresTemporaryAgent()
 
 const bcryptTemporaryAgent = new BcryptTemporaryAgent()
 
-const createTemporaryAgent = new CreateTemporaryAgent(postgresTemporaryAgent, bcryptTemporaryAgent)
+const emailMailTrapProvider = new EmailMailTrap()
+
+const createTemporaryAgent = new CreateTemporaryAgent(
+   postgresTemporaryAgent,
+   bcryptTemporaryAgent,
+   emailMailTrapProvider
+)
 
 const createTemporaryAgentController = new TemporaryAgentController(createTemporaryAgent)
 
