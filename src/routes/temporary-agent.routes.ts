59a8c8edd519aeab93ec69ec7027f3e71a0fea63
Capellinx <capellaaa7@gmail.com>
2024-08-02 21:17:05 -0300

    fix: change validate body to class

--- src/routes/temporary-agent.routes.ts
+++ src/routes/temporary-agent.routes.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { createTemporaryAgentController } from "../use-cases/create-temporary-agent";
 import { loginTemporaryAgentController } from "../use-cases/login-temporary-agent";
+import { loginTemporaryAgentSchema, registerTemporaryAgentSchema } from "../schemas/temporary-agent.schema";
+import { ValidateBody } from "../middlewares/validate-body.middleware";
 
 export const temporaryAgentRouter = Router()
 
-temporaryAgentRouter.post("/register", async (request, response) => {
-   return createTemporaryAgentController.handle(request, response)
-})
+temporaryAgentRouter.post("/register",
+   ValidateBody.execute({body: registerTemporaryAgentSchema}),
+   async (request, response) => {
+      return createTemporaryAgentController.handle(request, response)
+   }
+)
 
-temporaryAgentRouter.post("/login", async (request, response) => {
-   return loginTemporaryAgentController.handle(request, response)
-})
+temporaryAgentRouter.post("/login",
+   ValidateBody.execute({body: loginTemporaryAgentSchema}),
+   async (request, response) => {
+      return loginTemporaryAgentController.handle(request, response)
+   }
+)
