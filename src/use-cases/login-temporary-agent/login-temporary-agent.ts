4e8a1ff2af1e9227ba4a0e4e5d68138cd5254915
Capellinx <capellaaa7@gmail.com>
2024-08-02 21:59:12 -0300

    feat: create a new dependencie

--- src/use-cases/login-temporary-agent/login-temporary-agent.ts
+++ src/use-cases/login-temporary-agent/login-temporary-agent.ts
@@ -1,14 +1,14 @@
-import { PasswordAtuhGuard } from "../../auth/password.auth";
-import { TokenAuthGuard } from "../../auth/token.auth";
 import { ApiError } from "../../config/error/api.error";
 import { ILoginTemporaryAgentRequestDTO } from "../../domain/DTOs/temporaryAgent.DTO";
 import { IPasswordHash } from "../../interfaces/password.interface";
+import { IToken } from "../../interfaces/token.interface";
 import { ITemporaryAgentRepository } from "../../repositories/temporaryAgent.repository";
 
 export class LoginTemporaryAgentUseCase {
    constructor(
       private temporaryAgentRepository: ITemporaryAgentRepository,
-      private passwordHash: IPasswordHash
+      private passwordHash: IPasswordHash,
+      private token: IToken
    ) { }
 
    async execute({ email, password }: ILoginTemporaryAgentRequestDTO): Promise<any> {
@@ -24,7 +24,7 @@ export class LoginTemporaryAgentUseCase {
          throw new ApiError("Incorrect password", 401)
       }
       
-      const token = await TokenAuthGuard.generateToken(temporaryAgentExists.id)
+      const token = await this.token.generateToken(temporaryAgentExists.id)
 
       return {
          accessToken: token,
