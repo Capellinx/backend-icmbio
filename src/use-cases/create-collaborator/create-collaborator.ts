d96e698f390e51f571e97507e228217f6973176e
Capellinx <capellaaa7@gmail.com>
2024-09-21 20:20:35 -0300

    fix: change class create collaborator

--- src/use-cases/create-collaborator/create-collaborator.ts
+++ src/use-cases/create-collaborator/create-collaborator.ts
@@ -1,3 +1,4 @@
+import { Role } from "@prisma/client";
 import { CreateCollaboratorDTO } from "../../domain/dto/collaborator";
 import { Collaborator } from "../../domain/entities/collaborators";
 import { ApiError } from "../../error";
@@ -9,24 +10,25 @@ export class CreateCollaboratorUseCase {
       private collaboratorsRepository: ICollaboratorsRepository
    ) { }
 
-   async execute({ name, email, password, createdAt, person_type, cpf, phone }: CreateCollaboratorDTO): Promise<void> {
-      const collaborator = await this.collaboratorsRepository.findByEmail(email);
+   async execute(payload: CreateCollaboratorDTO): Promise<void> {
+      const collaborator = await this.collaboratorsRepository.findByEmail(payload.email);
 
       if (collaborator) throw new ApiError("Collaborator already exists", 400);
 
-      const newCollaborator = new Collaborator(
-         crypto.randomUUID(),
-         name,
-         email,
-         password,
-         createdAt,
-         person_type,
-         cpf,
-         phone
-      );
+      const newCollaborator = new Collaborator({
+         name: payload.name,
+         email: payload.email,
+         password: payload.password,
+         person_type: payload.person_type,
+         cpf: payload.cpf,
+         phone: payload.phone,
+         role: Role.USER,
+         createdAt: new Date(),
+         updatedAt: new Date()
+      });
 
       await this.collaboratorsRepository.save(newCollaborator)
 
-      return 
+      return
    }
 }
