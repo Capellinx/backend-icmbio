728d67abb43dceea628d2d7c4da06a20c7c029bf
Capellinx <capellaaa7@gmail.com>
2024-09-21 21:59:34 -0300

    refactor: remove the archives dont no use

--- src/services/collaborator.service.ts
+++
@@ -1,5 +0,0 @@
-import { CreateCollaboratorDTO } from "../domain/dto/collaborator";
-
-export interface ICollaboratorService {
-   create({ name, email, password, createdAt, person_type, cpf, phone }: CreateCollaboratorDTO): Promise<void>
-}
