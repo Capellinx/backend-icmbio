170df89a94ea4a0f205cc51dc06133eafffbe10b
Capellinx <capellaaa7@gmail.com>
2024-09-22 01:47:11 -0300

    feat: add cors to app use

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import express, { json } from "express";
 import { HandleErros } from "./middleware/handle-errors.middleware";
 import helmet from "helmet";
 import { collaboratorRouter } from "./routes/collaborator";
+import cors from "cors"
 
 export const app = express()
+
+
+app.use(cors({
+   origin: "http://localhost:3000",
+   methods: ["GET", "POST", "PUT", "DELETE"],
+}))
 
 app.use(helmet())
 
