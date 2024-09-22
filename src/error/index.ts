88cf8424a8dbfa6fc24f3f165984b26bf47fc2d8
Capellinx <capellaaa7@gmail.com>
2024-09-22 00:18:51 -0300

    fix: remove type in statusCode

--- src/error/index.ts
+++ src/error/index.ts
@@ -2,7 +2,7 @@
 export class ApiError extends Error {
    public statusCode: number
 
-   constructor(message: string, statusCode: number = 400) {
+   constructor(message: string, statusCode = 400) {
       super(message);
       this.statusCode = statusCode
    }
