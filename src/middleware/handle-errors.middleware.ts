0be858aba15d89a4b63dda42c19227ab2ed42aaf
Capellinx <capellaaa7@gmail.com>
2024-09-22 00:19:29 -0300

    feat: improve more control errors in application

--- src/middleware/handle-errors.middleware.ts
+++ src/middleware/handle-errors.middleware.ts
@@ -1,13 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../error";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { ZodError } from "zod";
 
 
 export class HandleErros {
-   constructor(){}
+   constructor() { }
 
-   static execute(error: Error, request: Request, response: Response, next: NextFunction) {
-      if(error instanceof ApiError){
-         return response.status(error.statusCode).json({ message: error.message });
+   static execute(error: ApiError | Error, req: Request, res: Response, next: NextFunction) {
+      if (error instanceof ApiError) {
+         return res.status(error.statusCode).json({ error: error.message });
       }
+
+      if (error instanceof PrismaClientKnownRequestError) {
+         return res.status(500).json({ error: 'Internal Server Error.' });
+      }
+
+      if (error instanceof ZodError) {
+         return res.status(500).json(error.errors);
+      }
+
+      return res.status(500).json({ message: "Internal Server Error." })
    }
 }
