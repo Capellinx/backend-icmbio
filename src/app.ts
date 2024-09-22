8d9dc0b0cd54a84a07b3c82fbb8482749f74e0eb
Capellinx <capellaaa7@gmail.com>
2024-09-21 21:58:49 -0300

    feat: create route to register collaborator

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@
 import express, { json } from "express";
 import { HandleErros } from "./middleware/handle-errors.middleware";
 import helmet from "helmet";
+import { collaboratorRouter } from "./routes/collaborator";
 
 export const app = express()
 
 app.use(helmet())
 app.use(json())
 app.use(HandleErros.execute)
+
+app.use(collaboratorRouter)
