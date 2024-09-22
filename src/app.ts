239645b6586a79e5695f06f7b3b15057ee9d8c0d
Capellinx <capellaaa7@gmail.com>
2024-09-22 00:03:30 -0300

    feat: add express async errors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 
+import "express-async-errors"
 import express, { json } from "express";
 import { HandleErros } from "./middleware/handle-errors.middleware";
 import helmet from "helmet";
@@ -7,7 +8,9 @@ import { collaboratorRouter } from "./routes/collaborator";
 export const app = express()
 
 app.use(helmet())
+
 app.use(json())
-app.use(HandleErros.execute)
 
 app.use(collaboratorRouter)
+
+app.use(HandleErros.execute)
