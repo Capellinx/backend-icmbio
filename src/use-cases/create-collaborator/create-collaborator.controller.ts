bb031c96d2965f0a0ab95ad59782ee336938e0eb
Capellinx <capellaaa7@gmail.com>
2024-09-22 00:19:59 -0300

    fix: change response of controller

--- src/use-cases/create-collaborator/create-collaborator.controller.ts
+++ src/use-cases/create-collaborator/create-collaborator.controller.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from "express";
 import { CreateCollaboratorUseCase } from "./create-collaborator";
+import { Role } from "@prisma/client";
 
 
 export class CreateCollaboratorController {
    constructor(
       private createCollaboratorUseCase: CreateCollaboratorUseCase
-   ){}
+   ) { }
 
    async handle(request: Request, response: Response) {
-      const { name, email, password, createdAt, person_type, cpf, phone } = request.body;
+      const { name, email, password, person_type, cpf, phone, role } = request.body;
 
-      await this.createCollaboratorUseCase.execute({
+      const { success, id } = await this.createCollaboratorUseCase.execute({
          name,
          email,
          password,
-         createdAt,
          person_type,
          cpf,
+         role,
          phone
       })
 
-      return response.status(201).send()
+      return response.status(201).json({
+         id,
+         success,
+      })
    }
 }
