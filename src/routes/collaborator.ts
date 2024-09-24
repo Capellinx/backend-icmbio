83b3b20dbe00c3d31866ed65692c5276a4b6e129
Capellinx <capellaaa7@gmail.com>
2024-09-23 22:30:57 -0300

    feat: create new route to login collaborator

--- src/routes/collaborator.ts
+++ src/routes/collaborator.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { createCollaboratorController } from "../use-cases/create-collaborator";
 import { ZodRequestValidate } from "../middleware/zod-errors.middleware";
 import { createCollaboratorSchema } from "../schemas/create-collaborator";
+import { loginCollaboratorController } from "../use-cases/collaborator/login";
+import { loginCollaboratorSchema } from "../schemas/login-collaborator";
 
 
 export const collaboratorRouter = Router()
@@ -11,5 +13,13 @@ collaboratorRouter.post(
    ZodRequestValidate.execute({ body: createCollaboratorSchema }),
    async (request, response) => {
       return await createCollaboratorController.handle(request, response)
+   }
+)
+
+collaboratorRouter.post(
+   "/login",
+   ZodRequestValidate.execute({ body: loginCollaboratorSchema }),
+   async (request, response) => {
+      return await loginCollaboratorController.handle(request, response)
    }
 )
