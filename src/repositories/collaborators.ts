264e67073b06cfa8b2810f35eff4b34e9c6a8809
Capellinx <capellaaa7@gmail.com>
2024-09-23 21:03:29 -0300

    feat: update repository

--- src/repositories/collaborators.ts
+++ src/repositories/collaborators.ts
@@ -3,6 +3,7 @@ import { CreateCollaboratorDTO } from "../domain/dto/collaborator";
 export interface ICollaboratorsRepository {
    save({ name, email, password, person_type, cpf, phone }: CreateCollaboratorDTO): Promise<void>
    findByEmail(email: string): Promise<any>
+   existCpf(cpf: string): Promise<boolean>
 }
 
 export namespace ICollaboratorsRepository {
