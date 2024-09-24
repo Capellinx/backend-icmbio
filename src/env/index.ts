fa5970b22df3b61fc251e95b1ab832bb71aa61b9
Capellinx <capellaaa7@gmail.com>
2024-09-23 21:21:16 -0300

    feat: update env with new secrets

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 const envSchema = z.object({
    PORT: z.coerce.number().describe('The port the server will listen on').default(8443),
    DATABASE_URL: z.string().describe('The string connection to the database').default('postgres://postgres:postgres@localhost:5432/postgres'),
+   JWT_SECRET: z.string().describe('The secret used to sign the JWT'),
+   JWT_REFRESH_SECRET: z.string().describe('The secret used to sign the refresh JWT'),
 });
 
 const _env = envSchema.safeParse(process.env);
