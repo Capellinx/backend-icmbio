b3c96524b56b8c4e69088b4a73708aaf8b5c8f3f
Capellinx <capellaaa7@gmail.com>
2024-09-23 21:16:36 -0300

    refactor: remove controller because dont use more

--- src/use-cases/collaborator/check-cpf-exist/index.ts
+++ src/use-cases/collaborator/check-cpf-exist/index.ts
@@ -1,12 +1,9 @@
 
 import { PostgresCollaboratorService } from '../../../repositories/implementations/postgres-collaborator';
 import { CheckCpfExistUseCase } from './check-cpf-exist';
-import { CheckCpfController } from './check-cpf-exist.controller';
 
 const postgresCollaborator = new PostgresCollaboratorService()
 
 const checkCpfExistUseCase = new CheckCpfExistUseCase(postgresCollaborator)
 
-const checkCpfController = new CheckCpfController(checkCpfExistUseCase)
-
-export { checkCpfController, checkCpfExistUseCase }
+export { checkCpfExistUseCase }
