9f106e9a22e19a023ecdde6f00923d2b41e8bc4c
Capellinx <capellaaa7@gmail.com>
2024-10-02 21:25:02 -0300

    feat: create use case in new folder

---
+++ src/use-cases/collaborator/create-collaborator/index.ts
@@ -0,0 +1,19 @@
+import { PostgresCollaboratorService } from "../../../repositories/implementations/postgres-collaborator";
+import { NodeMailerService } from "../../../services/implementations/nodemailer.service";
+import { checkCpfExistUseCase } from "../check-cpf-exist";
+import { CreateCollaboratorUseCase } from "./create-collaborator";
+import { CreateCollaboratorController } from "./create-collaborator.controller";
+
+const postgresCollaborator = new PostgresCollaboratorService()
+
+const nodemailerService = new NodeMailerService()
+
+const createCollaboratorUseCase = new CreateCollaboratorUseCase(
+   postgresCollaborator,
+   checkCpfExistUseCase,
+   nodemailerService
+)
+
+const createCollaboratorController = new CreateCollaboratorController(createCollaboratorUseCase)
+
+export { createCollaboratorController }
