22336480105dd859dfd36ccc4798096dd4d8b6b7
Capellinx <capellaaa7@gmail.com>
2024-10-02 22:23:13 -0300

    feat: instance the bcrypt service

--- src/use-cases/collaborator/create-collaborator/index.ts
+++ src/use-cases/collaborator/create-collaborator/index.ts
@@ -1,4 +1,5 @@
 import { PostgresCollaboratorService } from "../../../repositories/implementations/postgres-collaborator";
+import { BcryptService } from "../../../services/implementations/bcrypt.service";
 import { NodeMailerService } from "../../../services/implementations/nodemailer.service";
 import { checkCpfExistUseCase } from "../check-cpf-exist";
 import { CreateCollaboratorUseCase } from "./create-collaborator";
@@ -8,10 +9,13 @@ const postgresCollaborator = new PostgresCollaboratorService()
 
 const nodemailerService = new NodeMailerService()
 
+const bcryptService = new BcryptService()
+
 const createCollaboratorUseCase = new CreateCollaboratorUseCase(
    postgresCollaborator,
    checkCpfExistUseCase,
-   nodemailerService
+   nodemailerService,
+   bcryptService
 )
 
 const createCollaboratorController = new CreateCollaboratorController(createCollaboratorUseCase)
