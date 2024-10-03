56a723f33e63279a760670c16bc6cf598767baf2
Capellinx <capellaaa7@gmail.com>
2024-10-02 21:49:10 -0300

    feat: create new functionality to find id in repository

--- src/repositories/collaborators.ts
+++ src/repositories/collaborators.ts
@@ -3,10 +3,21 @@ export interface ICollaboratorsRepository {
    save({ name, email, password, person_type, cpf, phone }: CreateCollaboratorDTO): Promise<void>
    findByEmail(email: string): Promise<any>
    existCpf(cpf: string): Promise<boolean>
+   findById(id: string): Promise<ICollaboratorsRepository.FindByIdOutput | null>
 }
 
 export namespace ICollaboratorsRepository {
    export type FindByEmailOutput = {
       email: string
+   }
+
+   export type FindByIdOutput = {
+      collaborator: {
+         id: string
+         name: string
+         email: string
+         person_type: string
+         phone: string
+      }
    }
 }
