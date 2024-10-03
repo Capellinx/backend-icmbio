b5d42041e35bf310ccc13cef98df19c2f1b67cc1
Capellinx <capellaaa7@gmail.com>
2024-10-02 21:17:37 -0300

    feat: injected new dependecie of email service

--- src/use-cases/create-collaborator/index.ts
+++ src/use-cases/create-collaborator/index.ts
@@ -1,17 +1,17 @@
 import { PostgresCollaboratorService } from "../../repositories/implementations/postgres-collaborator";
-import { BcryptService } from "../../services/implementations/bcrypt.service";
+import { NodeMailerService } from "../../services/implementations/nodemailer.service";
 import { checkCpfExistUseCase } from "../collaborator/check-cpf-exist";
 import { CreateCollaboratorUseCase } from "./create-collaborator";
 import { CreateCollaboratorController } from "./create-collaborator.controller";
 
 const postgresCollaborator = new PostgresCollaboratorService()
 
-const encryptPasswordService = new BcryptService()
+const nodemailerService = new NodeMailerService()
 
 const createCollaboratorUseCase = new CreateCollaboratorUseCase(
    postgresCollaborator,
-   encryptPasswordService,
-   checkCpfExistUseCase
+   checkCpfExistUseCase,
+   nodemailerService
 )
 
 const createCollaboratorController = new CreateCollaboratorController(createCollaboratorUseCase)
