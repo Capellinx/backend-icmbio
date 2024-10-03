5a172079aee88531152f4ec4194096acbc637135
Capellinx <capellaaa7@gmail.com>
2024-10-02 21:01:27 -0300

    feat: create new enviroment for send e-mails

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,6 +6,10 @@ const envSchema = z.object({
    DATABASE_URL: z.string().describe('The string connection to the database').default('postgres://postgres:postgres@localhost:5432/postgres'),
    JWT_SECRET: z.string().describe('The secret used to sign the JWT'),
    JWT_REFRESH_SECRET: z.string().describe('The secret used to sign the refresh JWT'),
+   EMAIL_HOST: z.string().describe('The email host'),
+   EMAIL_PORT: z.coerce.string().describe('The email port'),
+   EMAIL_USER: z.string().describe('The email user'),
+   EMAIL_PASS: z.string().describe('The email password'),
 });
 
 const _env = envSchema.safeParse(process.env);
